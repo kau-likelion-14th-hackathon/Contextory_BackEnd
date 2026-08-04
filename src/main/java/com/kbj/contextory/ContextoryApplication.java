32f45ca756d640f1b0bff681e7acf7f0df07a4fb
dh1180 <mjdh1936@gmail.com>
2026-08-05 02:43:51 +0900

    first commit

---
+++ src/main/java/com/kbj/contextory/ContextoryApplication.java
@@ -0,0 +1,13 @@
+package com.kbj.contextory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbj/contextory/ContextoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbj.contextory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
